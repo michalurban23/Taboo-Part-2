d64f31019fc29ed70f7279c8105f5715346d5c96
Michał Urban <michalurban23@gmail.com>
2017-07-19 15:19:43 +0200

    Refactors controller.py

--- controller.py
+++ controller.py
@@ -1,7 +1,6 @@
 from datetime import date
-# import date_handle
+from events import Event, Checkpoint, PrivateMentoring
 import view
-import events
 
 
 class Controller:
@@ -14,16 +13,20 @@ class Controller:
 
             if choice == "1":
                 self.book_private_mentoring()
+
             elif choice == "2":
                 self.book_checkpoint()
+
             elif choice == "3":
-                self.print_all_events()
+                self.display_all_events()
+
             else:
                 self.say_goodbye()
+                exit()
 
-    def print_all_events(self):
+    def display_all_events(self):
 
-        view.print_all_events(events.Event.get_events())
+        view.print_all_events(Event.get_events())
 
     def book_event(self):
 
@@ -32,14 +35,14 @@ class Controller:
     def book_checkpoint(self):
 
         date = view.get_event_date()
-        date = self.__class__.convert_date(date)
-        events.Checkpoint(date)
+        date = self.convert_date(date)
+        Checkpoint(date)
 
     def book_private_mentoring(self):
 
         date = view.get_event_date()
-        date = self.__class__.convert_date(date)
-        events.PrivateMentoring(date)
+        date = self.convert_date(date)
+        PrivateMentoring(date)
 
     def say_goodbye(self):
 
@@ -49,4 +52,4 @@ class Controller:
     def convert_date(date_str):
 
         date_list = date_str.split("-")
-        return (date(int(date_list[2]), int(date_list[1]), int(date_list[0])))
+        return date(int(date_list[2]), int(date_list[1]), int(date_list[0]))
